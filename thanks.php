ad6e6c70319ac3ae851411724ba6bd422d3ad046
taimomo <black.rush@100gmail.com>
2025-03-29 16:07:32 +0900

    contact series create

---
+++ thanks.php
@@ -0,0 +1,15 @@
+<?php
+$page_title = "CEM | 送信完了";
+include 'header.php';
+?>
+<main>
+    <section class="thanks hd_space">
+        <div class="container pd">
+            <h2>送信完了</h2>
+            <p>ありがとうございます。送信が完了しました。</p>
+            <p>追って担当者よりご連絡差し上げます。</p>
+        </div>
+    </section>
+</main>
+
+<?php include 'footer.php'; ?>
